50bc79b38ff721ad695ae2dfed6ce98cb06a2d75
Ahmed Sahim <sahimfingerstyle@gmail.com>
2016-03-18 09:54:29 +0100

    recuperation des champs dans les input

--- src/java/servlet/Model.java
+++ src/java/servlet/Model.java
@@ -6,10 +6,30 @@
 
 package servlet;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author sahmed
  */
 public class Model {
+    public Model(){}
+    
+    private String userAutorise;
+    private String mdpAutorise;
+        public void verifier_auth(HttpServletRequest request, HttpServletResponse response)
+        {
+            response.setContentType("text/html;charset=UTF-8");
+            String user = request.getParameter("user");
+            String mdp = request.getParameter("mdp");
+            
+            
+            userAutorise = "user";
+            mdpAutorise ="user";          
+            request.setAttribute("resultat",userAutorise);
+            request.setAttribute("resultat", mdpAutorise);
+            
+        }
     
 }
