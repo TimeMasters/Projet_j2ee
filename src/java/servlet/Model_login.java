56f7fad0b4d10ac9fec87f903b0243c726aba6b9
Ahmed Sahim <sahimfingerstyle@gmail.com>
2016-03-18 09:56:31 +0100

    merge + model

--- src/java/servlet/Model_login.java
+++ src/java/servlet/Model_login.java
@@ -6,10 +6,29 @@
 
 package servlet;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  *
  * @author sahmed
  */
 public class Model_login {
+    public Model_login(){}
     
+    private String userAutorise;
+    private String mdpAutorise;
+        public void verifier_auth(HttpServletRequest request, HttpServletResponse response)
+        {
+            response.setContentType("text/html;charset=UTF-8");
+            String user = request.getParameter("user");
+            String mdp = request.getParameter("mdp");
+            
+            
+            userAutorise = "user";
+            mdpAutorise ="user";          
+            request.setAttribute("resultat",userAutorise);
+            request.setAttribute("resultat", mdpAutorise);
+            
+        }
 }
